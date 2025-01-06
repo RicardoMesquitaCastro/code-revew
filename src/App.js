8335cf967b76c23de1582f0e5bb346c3d71b2b9f
Ricardo Mesquita <ricardomesquita12@hotmail.com>
2025-01-06 18:44:37 -0300

    props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
+import Item from './components/Item';
 
 function App() {
 
@@ -17,6 +19,9 @@ function App() {
       profissao="Programador"
       foto="https://via.placeholder.com/150"
       />
+        <List/>
+        <Item marca="Nikee" lancamento={1964}/>
+        <Item/>
     </div>
   );
 }
